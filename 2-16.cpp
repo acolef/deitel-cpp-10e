e1585a7980d89a0fb7dd307ebcfaa82a039f9260
acolef <79170209+acolef@users.noreply.github.com>
2021-02-16 15:57:36 -0500

    Update 2-16.cpp

--- 2-16.cpp
+++ 2-16.cpp
@@ -1,19 +1,22 @@
-/*This program asks for two numbers and then prints their
-sum, product, difference, and quotient.*/
+/* This program asks for two numbers and then prints their
+sum, product, difference, and quotient.
+ *
+ * Author: Alex Cole Foster
+ */
 
-#include <iostream>
+#include <iostream>                     //Pre-processor directive to include standard stream header
 using namespace std;
 
 int main(){
-    int first{0}, second{0};            //Declare and initialize two variables for input storag
+    int first{0}, second{0};            //Declare and initialize two variables for input storage
     int sum{0}, product{0},             //Declare and initialize the variables for storing the
     difference{0}, quotient{0},         //arithmetic operation values
     remainder{0};
     
-    cout << "Enter two numbers: ";
-    cin >> first >> second;
+    cout << "Enter two numbers: ";      //Prompt user for the inputs
+    cin >> first >> second;             //Accept inputs from the keyboard
     
-    sum = first + second;
+    sum = first + second;               //Relevant arithmetic
     product = first * second;
     difference = first - second;
     quotient = first / second;
@@ -27,3 +30,4 @@ int main(){
     
     return 0;
 }
+//The remainder was not asked for in the problem. It was done as extra practice using %.

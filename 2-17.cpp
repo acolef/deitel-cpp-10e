b91de88ea4a7e5ee950c927cb95a8f931f80564f
acolef <79170209+acolef@users.noreply.github.com>
2021-02-16 16:00:30 -0500

    Update 2-17.cpp

--- 2-17.cpp
+++ 2-17.cpp
@@ -1,14 +1,16 @@
 /* Prints the numbers 1-4 in numerous ways, with adjacent 
  * numbers separated by a single space.
+ *
+ * Author: Alex Cole Foster
  */
 
 #include <iostream>
 using namespace std;
 
 int main(){
-    cout << "1 2 3 4\n";
-    cout << "1 " << "2 " << "3 " << "4\n";
-    cout << "1 ";
+    cout << "1 2 3 4\n";                        //Print using one statement
+    cout << "1 " << "2 " << "3 " << "4\n";      //Print using multiple stream insertion operators
+    cout << "1 ";                               //Print using multiple statements
     cout << "2 ";
     cout << "3 ";
     cout << "4\n";
